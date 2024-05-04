6df37ca680226f4696e30a6759cda75425a6a4e9
Nishant <nishantnb258@gmail.com>
2024-05-04 20:03:32 +0530

    Home page completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React,{useRef,useEffect} from 'react'
 import logo from '../../assets/images/logo.png'
 import userImg from '../../assets/images/avatar-icon.png'
 import {NavLink,Link} from 'react-router-dom'
+import {BiMenu} from 'react-icons/bi'
 
 const navLinks = [
   {
@@ -23,16 +24,38 @@ const navLinks = [
 ]
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleStickyHeader = ()=>{
+    window.addEventListener('scroll', ()=> {
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        headerRef.current.classList.add('sticky__header')
+      }
+      else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(()=>{
+    handleStickyHeader();
+
+    return ()=> window.removeEventListener('scroll',handleStickyHeader);
+  })
+
+
+  const toggleMenu= ()=> menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
-          {/* Logo */}
           <div>
             <img src={logo} alt='' />
           </div>
 
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
           <ul className="menu flex items-center gap-[2.7rem]">
             {
               navLinks.map((link,index)=>{
@@ -54,7 +77,7 @@ const Header = () => {
           </div>
 
           <div className="flex items-center gap-4">
-            <div>
+            <div className="hidden">
               <Link to='/'>
                 <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
                   <img src={userImg} alt='' className="w-full rounded-full"/>
@@ -66,8 +89,8 @@ const Header = () => {
               <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]" >Login</button>
             </Link>
 
-            <span>
-              
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
             </span>
           </div>
         </div>
