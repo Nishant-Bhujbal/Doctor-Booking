c5cbed2d6cf7f74726977a13518d8293e095278d
Nishant <nishantnb258@gmail.com>
2024-05-05 18:19:44 +0530

    Doctors Page completed

---
+++ frontend/src/utils/FormatedDate.jsx
@@ -0,0 +1,7 @@
+export const FormatedDate = (date,config) => {
+    const defaultOptions = {day : 'numeric',month:'short',year : 'numeric'}
+    const options = config ? config : defaultOptions
+
+    return new Date(date).toLocaleDateString('en-US',options)
+
+}
