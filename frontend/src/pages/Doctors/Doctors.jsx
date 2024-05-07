85be6feeaa5da517dac2e8c1d83cef33c6e797d6
Nishant <nishantnb258@gmail.com>
2024-05-08 01:03:13 +0530

    doctor profile created

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -12,7 +12,7 @@ const Doctors = () => {
         <div className="max-w-[570px] mt-[30px] mx-auto bg-[#000666ff2c] rounded-md flex items-center justify-between">
           <input 
           type="search" 
-          className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer bg-gray-600 placeholder:text-white text-white" 
+          className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer bg-gray-400 placeholder:text-white text-white" 
           placeholder='Search Doctor' 
           />
           <button className="btn mt-0 rounded-[0px] rounded-r-md">
